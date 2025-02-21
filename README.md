04f2ce3f823fa53b658557a5c507a42de5d37a08
Alicia Boya García <aboya@igalia.com>
2025-02-21 09:01:26 +0100

    Recode counting.mp4 to h264

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-Quick and dirty scripts to generate MP4 with WebVTT files from the `counting.mp4`
-test vector from WebKit LayoutTests (`LayoutTests/media/content/counting.mp4`).
+Quick and dirty scripts to generate MP4 with WebVTT files.
+
+The original video file is `LayoutTests/media/content/counting.mp4` from WebKit
+(included here as `counting_mpeg4video.mp4`).
+
+To be compatible with more browsers (namely Firefox), I recoded it to h264 with
+the following command:
+
+```
+gst-launch-1.0 filesrc location=counting_mpeg4video.mp4 ! qtdemux ! decodebin ! x264enc ! mp4mux ! filesink location=counting_h264.mp4
+```
+
+`counting_h264.mp4` is used as base for the generated test vectors.
+
+# How to use
 
 Run:
 
--- generate_all.sh
+++ generate_all.sh
@@ -22,19 +22,23 @@ cd tmp
 
 "$DIR/generate_vtt.py" > simple.vtt
 
-# Non-fragmented, video and text
-cp ../counting.mp4 counting-vttsimple.mp4
-MP4Box -add simple.vtt counting-vttsimple.mp4
-cp counting-vttsimple.mp4 "$DIR/out/non-frag-counting-vttsimple.mp4"
+dash_input_prefix=counting-h264-vttsimple
+dash_input=$dash_input_prefix.mp4
 
-# Fragmented, video and text
-MP4Box -dash 5000 'counting-vttsimple.mp4'
-dash_to_mp4_with_manifest counting-vttsimple counting-video-vttsimple
+# Non-fragmented, video and text.
+# It will also be our dash_input for the next steps.
+cp ../counting_h264.mp4 $dash_input
+MP4Box -add simple.vtt $dash_input
+cp $dash_input "$DIR/out/non-frag-$dash_input"
 
-# Fragmented, only video
-MP4Box -dash 5000 'counting-vttsimple.mp4#trackID=1'
-dash_to_mp4_with_manifest counting-vttsimple counting-video
+# Fragmented, video and text.
+MP4Box -dash 5000 $dash_input
+dash_to_mp4_with_manifest $dash_input_prefix counting-h264-vttsimple
 
-# Fragmented, only text
-MP4Box -dash 5000 'counting-vttsimple.mp4#trackID=4'
-dash_to_mp4_with_manifest counting-vttsimple counting-vttsimple
+# Fragmented, only video.
+MP4Box -dash 5000 $dash_input'#trackID=1'
+dash_to_mp4_with_manifest $dash_input_prefix counting-h264
+
+# Fragmented, only text.
+MP4Box -dash 5000 $dash_input'#trackID=2'
+dash_to_mp4_with_manifest $dash_input_prefix counting-vttsimple
